13755c4b7db1a73a97e6f082937e85648aa18fc3
Chamod <chamodchathuranga2002@gmail.com>
2025-05-16 20:53:32 +0530

    build frontend and function

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { TicTacToe } from './Components/Assets/TicTacToe/TicTacToe.jsx'
+import './index.css' 
+
+function App() {
+  
+  return (
+    
+    <div>
+        <TicTacToe/>      
+    </div>
+  )
+}
+
+export default App
